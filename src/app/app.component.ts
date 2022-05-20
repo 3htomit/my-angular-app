faa7d9edfe6cc82e90846bd368b491750aa999d6
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-05-20 11:26:31 +0200

    Coding the base of team generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  // title = 'my-angular-app';
+  newMemberName = '';
+  members: string[] = [];
 }
